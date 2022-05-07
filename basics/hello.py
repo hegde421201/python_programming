459cca3e11fc657291526b9b309da3436e65b257
hegde421201 <55439785+hegde421201@users.noreply.github.com>
2022-05-07 21:58:54 +0530

    added array problem
    added array

--- basics/hello.py
+++ basics/hello.py
@@ -1,4 +1,3 @@
-
 '''
 print("hello world!!")
 print(4)
@@ -7,4 +6,52 @@ print(type(4))
 print(type(4.6))
 
 '''
+
+x = 44
+print(x + 55)
+
+
+def hegde():
+    print("Welcome Hegde")
+
+hegde()
+
+
+#inp = input("What is your name?\n")
+#print("Your name "+inp)
+
+print(type(True))
+
+x = 6
+y = 6
+
+if x < y:
+    print(" x less than y")
+elif x > y:
+    print("x greater than y")
+else:
+    print("x and y are equal")
+
+n = 7
+
+while n > 0:
+    print(n)
+    n = n - 1
+
+while True:
+    line = input('> ')
+    if line == 'done':
+        break
+    if line[0] == '#':
+        continue
+    print(line)
+
+people = ['Putin','Bush','Gorbachev']
+for president in people:
+    print(president," Happy",7)
+
+count = 0
+for i in [24,6,7,45,89]:
+    count = count + 2*i
+print('Count: ',count)
 
---
+++ basics/patterns.py
@@ -0,0 +1,27 @@
+'''
+
+
+
+largest = None
+
+for i in [113,5,53,56,2,89,-1]:
+    if largest is None or largest < i:
+        largest = i
+    print("Largest loop: ",largest)
+print("Largest ",largest)
+
+'''
+from typing import List
+
+fruit = 'watermelon'
+
+length = len(fruit)
+print(length)
+
+print(fruit[-2])
+print(fruit[1])
+print(fruit[0:5])
+print(fruit[:])
+print('melon' in fruit)
+
+print(fruit.find('a'))
